fa4fd4bc8183c1a82734ea647e3d926730a066b2
hoipranav <hoipranavv@gmail.com>
2024-06-27 01:20:05 +0530

    Update bot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from bot import HomePage, LoginPage
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
