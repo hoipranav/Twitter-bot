6851d61a0f066b8bd874a59d18e588133c1597d5
hoipranav <hoipranavv@gmail.com>
2024-07-02 02:39:36 +0530

    Update exe path

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from bot import HomePage, LoginPage
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-driver = webdriver.Chrome()
-
+driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
 homepage = HomePage(driver)
 loginpage = homepage.load_homepage()
 loginpage.login()
